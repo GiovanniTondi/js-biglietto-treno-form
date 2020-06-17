3e2810ccf0eff755defc1731ab0951fa12b6d1e7
Giovanni <tondigiovanni@gmail.com>
2020-06-17 18:34:47 +0200

    Aggiunto controllo dati inseriti

--- js/script.js
+++ js/script.js
@@ -24,35 +24,42 @@ btn.addEventListener("click", function() {
 
         console.log(nome, km, categoria);
 
-        // Verifico se bisogna scontare il prezzo
-        if (categoria == "Over65") {
-            prezzo = km * prezzokm * 0.6;
-        } else if (categoria == "Minorenne") {
-            prezzo = km * prezzokm * 0.8;
+        if ((km == "") || (nome == "")) {
+            alert("Tutti i campi devono essere compilati!");
+        } else if (km < 1) {
+            alert("Chilometri non validi");
         } else {
-            prezzo = km * prezzokm;
+            // Verifico se bisogna scontare il prezzo
+            if (categoria == "Over65") {
+                prezzo = km * prezzokm * 0.6;
+            } else if (categoria == "Minorenne") {
+                prezzo = km * prezzokm * 0.8;
+            } else {
+                prezzo = km * prezzokm;
+            }
+            console.log(prezzo);
+
+            // Genero numero treno e carrozza
+            numTreno = Math.floor(Math.random() * (maxNumTreno - minNumTreno +1) + minNumTreno);
+            carrozza = Math.floor(Math.random() * (maxCarrozza - minCarrozza +1) + minCarrozza);
+            console.log(carrozza);
+
+            // Inserisco i valori
+            document.getElementById('passeggero').innerHTML = nome;
+            document.getElementById('numTreno').innerHTML = numTreno;
+            document.getElementById('nCarrozza').innerHTML = carrozza;
+            document.getElementById('prezzo').innerHTML = "€" + prezzo.toFixed(2);
+            document.getElementById('tariffa').innerHTML = categoria;
+
+            // Animazioni
+            document.getElementById('titolo').innerHTML = "Ecco il tuo biglietto!";
+            document.getElementById('form').className = "hidden";
+            document.getElementById('biglietto').className = "show";
+
+            // Cambio stato button
+            document.getElementById('btnCrea').innerHTML = "Annulla";
+
         }
-        console.log(prezzo);
-
-        // Genero numero treno e carrozza
-        numTreno = Math.floor(Math.random() * (maxNumTreno - minNumTreno +1) + minNumTreno);
-        carrozza = Math.floor(Math.random() * (maxCarrozza - minCarrozza +1) + minCarrozza);
-        console.log(carrozza);
-
-        // Inserisco i valori
-        document.getElementById('passeggero').innerHTML = nome;
-        document.getElementById('numTreno').innerHTML = numTreno;
-        document.getElementById('nCarrozza').innerHTML = carrozza;
-        document.getElementById('prezzo').innerHTML = "€" + prezzo.toFixed(2);
-        document.getElementById('tariffa').innerHTML = categoria;
-
-        // Animazioni
-        document.getElementById('titolo').innerHTML = "Ecco il tuo biglietto!";
-        document.getElementById('form').className = "hidden";
-        document.getElementById('biglietto').className = "show";
-
-        // Cambio stato button
-        document.getElementById('btnCrea').innerHTML = "Annulla";
 
     } else if (statoBtn == "Annulla") {
 
