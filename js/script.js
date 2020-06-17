7daffe374e0514086d1870ec36118903db16168b
Giovanni <tondigiovanni@gmail.com>
2020-06-17 15:00:33 +0200

    Starter template

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+    Calcolare il prezzo del biglietto in base ai km,
+    prendendo i dati da un form e stampare il biglietto.
+*/
+
+var prezzokm = 0.21;
+var btn = document.getElementById('btnCrea');
+
+
+
+btn.addEventListener("click", function() {
+    
+});
